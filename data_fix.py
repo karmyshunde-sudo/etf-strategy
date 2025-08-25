8cfec9e390251043ca9be4dfc934fdbdf25f7ca8
karmyshunde-sudo <karmy.shunde@gmail.com>
2025-08-25 16:59:49 +0800

    Update data_fix.py

--- data_fix.py
+++ data_fix.py
@@ -1490,3 +1490,21 @@ def mark_new_stock_info_pushed():
     with open(flag_path, 'w') as f:
         f.write(get_beijing_time().strftime('%Y-%m-%d %H:%M:%S'))
     logger.info(f"标记新股信息已推送: {flag_path}")
+
+def read_listing_pushed_flag(date):
+    """读取新上市交易信息是否已推送标志
+    参数:
+        date: 日期对象
+    返回:
+        tuple: (flag_path, is_pushed)
+    """
+    flag_path = os.path.join(Config.NEW_STOCK_DIR, f'listing_pushed_{date.strftime("%Y%m%d")}.flag')
+    is_pushed = os.path.exists(flag_path)
+    return flag_path, is_pushed
+
+def mark_listing_info_pushed():
+    """标记新上市交易信息已推送"""
+    flag_path, _ = read_listing_pushed_flag(get_beijing_time().date())
+    with open(flag_path, 'w') as f:
+        f.write(get_beijing_time().strftime('%Y-%m-%d %H:%M:%S'))
+    logger.info(f"标记新上市交易信息已推送: {flag_path}")
